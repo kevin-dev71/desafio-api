b592d028d0417c233a4ca24674f23e3136b6fd55
Kevin Dev <kevin.developer71@gmail.com>
2021-07-18 01:02:46 -0600

    feat: auth with JWT and middlewares

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 
+import {createRoles} from './libs/initialSetup'
+
 import productsRoutes from './routes/products.routes'
 import authRoutes from './routes/auth.routes'
+import userRoutes from './routes/user.routes'
 
 const app = express()
+createRoles() // aad database with the admin role and user role for User Model
 
 app.use(morgan('dev'))
 
@@ -22,5 +26,6 @@ app.get('/' , (req, res) => {
 
 app.use('/api/products', productsRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
 
 export default app
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,8 @@
 import { Router } from 'express'
 const router = Router()
+import * as userController from '../controllers/user.controller'
+import { authJwt, verifySignup } from '../middlewares'
+
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin, verifySignup.checkRolesExisted], userController.createUser)
 
 export default router
